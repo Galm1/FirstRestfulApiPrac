f89db3e73f91931ff60e5cb957066f9f6788cbf2
EliGarcia <garciaelisha93@yahoo.com>
2017-09-26 08:44:15 -0500

    first commit

---
+++ src/main/java/com/example/restfulAPIPrac/RestfulApiPracApplication.java
@@ -0,0 +1,12 @@
+package com.example.restfulAPIPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiPracApplication.class, args);
+	}
+}
